c2fb32e4d986a8ab1dd3b14a3d4c2b8ba5ded464
octubre1100 <37826866@ivt.edu.ar>
2023-11-27 10:44:18 -0300

    cambios
    juani

--- dom.js
+++ dom.js
@@ -1,7 +1,21 @@
 let imagen = document.querySelector('img')
-let boton = document.querySelector('#botoncolor')
+let botonTurquesa = document.querySelector('#botonTurquesa')
+let botonRojo = document.querySelector('#botonRojo')
+let botonBlanco = document.querySelector('#botonBlanco')
+let botonNegro = document.querySelector('#botonNegro')
 
-boton.onclick = function(){
+botonTurquesa.onclick = function(){
     imagen.src = 'iphone 11 turquesa.jpg'
+}
 
+botonRojo.onclick = function(){
+    imagen.src = 'ihpon 11 rojo.jfif'
+}
+
+botonBlanco.onclick = function(){
+    imagen.src = 'ihpone 11 blanco.jpg'
+}
+
+botonNegro.onclick = function(){
+    imagen.src = 'ihpone negro.png'
 }
